1f25355054b3abd54efe09810d3001d30a84283e
francisct <franciscotetremblay@gmail.com>
2017-01-30 14:13:52 -0500

    git ignore + initial commit

---
+++ AIevader/Assets/Scripts/PlayerUnit.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerUnit : MonoBehaviour
+{
+    NavMeshAgent navMeshAgent;
+
+    void Start ()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    public void SetDestination(Vector3 location)
+    {
+        navMeshAgent.SetDestination(location);
+    }
+}
