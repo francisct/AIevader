89f10bff54faaf112bcf81e23b1bd396e551ad03
Relf <winglong218@hotmail.com>
2017-03-23 19:04:32 -0400

    Added AIManager

--- AIevader/Assets/Scripts/AI/AIManager.cs
+++ AIevader/Assets/Scripts/AI/AIManager.cs
@@ -3,14 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AIManager : MonoBehaviour {
+    public AIController[] AIs;
+    private List<AIController> commandableAIs = new List<AIController>();
+    private List<AIController> busyAIs = new List<AIController>();
+    private GameObject player;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start () {
+        InitializeLists();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+    private void InitializeLists()
+    {
+        commandableAIs.AddRange(AIs);
+    }
+    /// <summary>
+    /// Mark an AI as busy
+    /// </summary>
+    /// <param name="ai"></param>
+    public void OccupiedAI(AIController ai)
+    {
+        commandableAIs.Remove(ai);
+        busyAIs.Add(ai);
+    }
+    /// <summary>
+    /// Mark an AI as commandable
+    /// </summary>
+    /// <param name="ai"></param>
+    public void FreeAI(AIController ai)
+    {     
+        busyAIs.Remove(ai);
+        commandableAIs.Add(ai);
+    }
 }
