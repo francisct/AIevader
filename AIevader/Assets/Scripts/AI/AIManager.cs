d83a4198fe4d81831ca7057ada39ab4ea6b2f518
Relf <winglong218@hotmail.com>
2017-03-23 19:10:51 -0400

    Added code to find nearest AI and to send an AI to chase

--- AIevader/Assets/Scripts/AI/AIManager.cs
+++ AIevader/Assets/Scripts/AI/AIManager.cs
@@ -2,22 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AIManager : MonoBehaviour {
+public class AIManager : MonoBehaviour
+{
     public AIController[] AIs;
     private List<AIController> commandableAIs = new List<AIController>();
     private List<AIController> busyAIs = new List<AIController>();
     private GameObject player;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         InitializeLists();
         player = GameObject.FindGameObjectWithTag("Player");
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
     private void InitializeLists()
     {
         commandableAIs.AddRange(AIs);
@@ -36,8 +39,37 @@ public class AIManager : MonoBehaviour {
     /// </summary>
     /// <param name="ai"></param>
     public void FreeAI(AIController ai)
-    {     
+    {
         busyAIs.Remove(ai);
         commandableAIs.Add(ai);
+    }
+    /// <summary>
+    /// Find the closest available AI to Gameobject g
+    /// </summary>
+    /// <param name="g"></param>
+    /// <returns></returns>
+    private AIController FindClosestAvailableAI(GameObject g)
+    {
+        AIController closestAI = null;
+        float closestDistance = Mathf.Infinity;
+
+        foreach (AIController ai in commandableAIs)
+        {
+
+            float distance = (ai.transform.position - g.transform.position).magnitude;
+            if (distance < closestDistance)
+            {
+                closestAI = ai;
+            }
+
+        }
+
+        return closestAI;
+    }
+
+    private void SendAIToChase(AIController ai)
+    {
+        OccupiedAI(ai);
+        ai.aiRole = AIController.role.Chase;
     }
 }
