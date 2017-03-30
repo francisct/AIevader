40b993b1be6082b5171182c12e856413322adeca
francisct <franciscotetremblay@gmail.com>
2017-03-29 21:51:54 -0400

    added skeleton classes

---
+++ AIevader/Assets/Scripts/Combat/ExpandexRemote.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExpandexRemote : MonoBehaviour {
+
+    ExpandexController expandex;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Activate()
+    {
+
+    }
+
+    void Deactivate()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+         //check for collision with GrowMe cube
+    }
+}
