aab26b83f1afe67f7ca9cf48f298fc3a64fa92e6
francisct <franciscotetremblay@gmail.com>
2017-04-10 21:56:21 -0400

    added healthbar

---
+++ AIevader/Assets/Scripts/Player/HealthController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthController : MonoBehaviour {
+
+    [SerializeField]
+    private int health;
+    private int initialHealth;
+    private GUIBarScript healthBar;
+	// Use this for initialization
+	void Start () {
+        initialHealth = health;
+        healthBar = GameObject.Find("HealthBar").GetComponent<GUIBarScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    bool TakeDamage(int damage)
+    {
+        health -= damage;
+        healthBar.Value = health / initialHealth;
+        return health <= 0;
+    }
+}
