e728612fc5b5273d01db26dd3b12b7b7912cc926
Relf <winglong218@hotmail.com>
2017-04-12 00:19:17 -0400

    Fixed bug from gizmo

--- AIevader/Assets/Scripts/Pathfinding/Grid.cs
+++ AIevader/Assets/Scripts/Pathfinding/Grid.cs
@@ -100,20 +100,13 @@ public class Grid : MonoBehaviour
     }
     void OnDrawGizmos()
     {
-        var a = GameObject.FindObjectOfType<AIController>();
+
         Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
         if (grid != null)
         {
             foreach (Node n in grid)
             {
                 Gizmos.color = n.isWalkable ? Color.white : Color.red;
-                if (a.path != null)
-                {
-                    if (a.path.Contains(n))
-                    {
-                        Gizmos.color = Color.black;
-                    }
-                }
                 Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - 0.1f));
             }
         }
